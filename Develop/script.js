c15915e2d886b47ce09e3df9e282c6e615f1109d
Milovets <elizabeth.milovets@gmail.com>
2021-06-17 23:30:56 -0500

    created criteria object and prompted for all values

--- Develop/script.js
+++ Develop/script.js
@@ -13,5 +13,43 @@ function writePassword() {
 
 }
 
+var passLength = function() {
+  var length = window.prompt("How many characters should your password be? min 8 characters, max 128 characters.");
+  if (length <= 128 && length >= 8){
+    return length;
+  }else {
+    window.alert("Not a valid entry, try again...");
+    passLength();
+  }
+}
+
+
+function generatePassword() {
+  var criteria = {
+    length: 0,
+    lowercase: false,
+    uppercase: false,
+    numeric: false,
+    special: false
+  };
+
+  criteria.length = passLength();
+  criteria.lowercase = window.confirm("Do you want to use lowercase letters in your password?");
+  criteria.uppercase = window.confirm("Do you want to use uppercase letters in your password?");
+  criteria.numeric = window.confirm("Do you want to use numeric characters in your password?");
+  criteria.special = window.confirm("Do you want to use special characters in your password?");
+
+  if (
+    criteria.lowercase === false &&
+    criteria.uppercase === false &&
+    criteria.numeric === false &&
+    criteria.special === false
+  ) {
+    window.alert("You must select at least one character type for your password.");
+    generatePassword();
+  }
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
